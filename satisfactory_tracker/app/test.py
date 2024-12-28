cf2f8617851076ac8a9958ab3ea77e163dfa3bc0
Catherine van Dijk <catherine.vandijk@gmail.com>
2024-12-28 08:07:00 +0000

    Updates to useful_stuff Fresh build on laptop

--- satisfactory_tracker/app/test.py
+++ satisfactory_tracker/app/test.py
@@ -1,18 +1,1 @@
-#TODO: If you need dynamic filtering based on user input
-from app import db
-from sqlalchemy.sql import text
-
-filters = []
-params = {}
-if user_input_part_name:
-    filters.append("part_name = :part_name")
-    params["part_name"] = user_input_part_name
-if user_input_category:
-    filters.append("category = :category")
-    params["category"] = user_input_category
-
-where_clause = " AND ".join(filters)
-query = text(f"SELECT * FROM parts WHERE {where_clause}")
-result = db.session.execute(query, params).fetchall()
-
 
